edc2d29b41f16478be76340e2706a178cf203763
YuanAnh <fakeloove119@gmail.com>
2025-03-17 15:45:45 +0700

    realphoneA

---
+++ Config/ketnoi.php
@@ -0,0 +1,11 @@
+<?php
+$conn = mysqli_connect('sql105.infinityfree.com', 'if0_38400451','underware211', 'if0_38400451_realphone');
+ if (!$conn)
+      {
+	die('Không thể kết nối: ' . mysqli_connect_error());
+      }
+ else
+      {
+        echo '';
+      }
+?>
