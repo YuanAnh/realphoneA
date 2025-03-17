646df587a4f644aaa372a7a676e86764bd2a445c
TuanAinh <148334441+YuanAnh@users.noreply.github.com>
2025-03-17 23:45:22 +0700

    Update connect.php

--- ADMIN/connect.php
+++ ADMIN/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('sql105.infinityfree.com', 'if0_38400451','underware211', 'if0_38400451_realphone');
+$conn = mysqli_connect('localhost', 'root','', 'webbanhang');
  if (!$conn)
       {
 	die('Không thể kết nối: ' . mysqli_connect_error());
